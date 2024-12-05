c00dbe28d5c3b881b8fa45341d43ee02658fbe30
dylanisyess <dylanlin2009@gmail.com>
2024-12-05 10:31:32 -0800

    did PID for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_auto.java
@@ -3,46 +3,76 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+// info
+// 357 encoder counts for one tile (2 feet or 24 inches)
 
 @Config
-@TeleOp(name="PID_auto", group="Linear OpMode")
+
+@Autonomous(name="Robot left", group="Robot")
 //@Disabled
-public class PID_auto extends OpMode{
+public class PID_auto extends LinearOpMode {
     private PIDController controller;
 
-    public static double p = 0, i = 0, d = 0;
+    public static double Kp = 0, Ki = 0, Kd = 0;
     public static double f = 0;
     public static int target = 0;
     private final double ticks_in_degree = 2000 / 270.0;
 
-    private DcMotor         leftwheel   = null;
-    private DcMotor         rightwheel  = null;
+    private DcMotorEx       leftwheel   = null;
+    private DcMotorEx       rightwheel  = null;
     private Servo           leftservo   = null;
     private Servo           rightservo  = null;
     private Servo           grabber     = null;
     private Servo           tilt        = null;
     private Servo           arm         = null;
+    double integralSum;
+    private double lasterror = 0;
+    ElapsedTime timer = new ElapsedTime();
 
-    @Override
-    public void init() {
-        controller = new PIDController(p, i, d);
+//    @Override
+    public void runOpMode() {
+        controller = new PIDController(Kp, Ki, Kd);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        leftwheel  = hardwareMap.get(DcMotor.class, "leftwheel");
-        rightwheel = hardwareMap.get(DcMotor.class, "rightwheel");
+        leftwheel  = hardwareMap.get(DcMotorEx.class, "leftwheel");
+        rightwheel = hardwareMap.get(DcMotorEx.class, "rightwheel");
         leftservo = hardwareMap.get(Servo.class, "leftservo");
         rightservo = hardwareMap.get(Servo.class, "rightservo");
         grabber  = hardwareMap.get(Servo.class, "grabber");
         tilt = hardwareMap.get(Servo.class, "tilt");
         arm = hardwareMap.get(Servo.class, "arm");
+        leftwheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightwheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftwheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightwheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+
+        double power = PIDcontrol(357, leftwheel.getCurrentPosition());
+        leftwheel.setPower(power);
+        rightwheel.setPower(power);
+
     }
 
-    @Override
-    public void loop() {
-        controller.setPID(p, i, d);
+//    @Override
+    public double PIDcontrol(double reference, double state) {
+        double error = reference - state;
+        integralSum += error * timer.seconds();
+        double derivative = (error - lasterror) / timer.seconds();
+        lasterror = error;
+
+        timer.reset();
+
+        double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki);
+        return output;
     }
 }
